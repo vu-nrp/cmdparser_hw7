a750fb807f1c7d121f1cd384438225bf9ae9375b
usov <spb-public@yandex.ru>
2025-05-18 19:15:17 +0300

    for doxygen

--- utils.h
+++ utils.h
@@ -2,8 +2,5 @@
 
 #include "types.h"
 
-//!
-//! \brief printAndClearCommands - print command bloc content and clear block
-//!
-
+//
 void printAndClearCommands(const size_t &startTime, CommandsPack &pack);
